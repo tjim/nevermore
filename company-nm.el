9c67862260247fe1afe3107157d75745390bc292
Trevor Jim <tjim@mac.com>
2014-06-23 12:05:54 -0400

    Move address completion code out to company-nm.el

---
+++ company-nm.el
@@ -0,0 +1,32 @@
+; Mail address completion for nevermore via company-mode
+
+; NB company-minimum-prefix-length defaults to 3 so you don't get completion unless you type 3 characters
+
+(require 'company)
+(require 'message)
+;(eval-when-compile (require 'cl))
+
+(defvar-local company-nm-last-prefix nil)
+;;;###autoload
+(defun company-nm (command &optional arg &rest ignore)
+  "`company-mode' completion back-end for `nevermore (nm)'."
+  (interactive (list 'interactive))
+  (let ((case-fold-search t))
+    (pcase command
+      (`interactive (company-begin-backend 'company-nm))
+      (`prefix (and (eq major-mode 'message-mode)
+                    (looking-back "^\\(To\\|Cc\\|Bcc\\):.*"
+                                  (line-beginning-position))
+                    (setq company-nm-last-prefix (company-grab-symbol))))
+      (`candidates (let ((results (completion-substring--all-completions arg nm-completion-addresses nil 0)))
+                     (when results (car results))))
+      (`match (if (string-match company-nm-last-prefix arg)
+                  (match-end 0)
+                0))
+      (`no-cache t))))
+
+(add-hook 'message-mode-hook '(lambda ()
+                                (company-mode)
+                                (make-local-variable 'company-backends)
+                                (setq company-backends '(company-nm))))
+(provide 'company-nm)
--- nm.el
+++ nm.el
@@ -1045,8 +1045,7 @@ Turning on `nm-mode' runs the hook `nm-mode-hook'.
   (run-mode-hooks 'nm-mode-hook)
   (add-hook 'post-command-hook 'nm-results-post-command nil t)
   (when (not nm-completion-addresses) (nm-async-harvest))
-  (nm-refresh)
-  )
+  (nm-refresh))
 
 ;;;###autoload
 (defun nm ()
@@ -1055,35 +1054,5 @@ Turning on `nm-mode' runs the hook `nm-mode-hook'.
   (switch-to-buffer nm-results-buffer)
   (if (not (eq major-mode 'nm-mode))
       (nm-mode)))
-
-;;; completion
-;;; NB company-minimum-prefix-length defaults to 3 so you don't get completion unless you type 3 characters
-(require 'company)
-;(eval-when-compile (require 'cl))
-
-(defvar-local company-nm-last-prefix nil)
-;;;###autoload
-(defun company-nm (command &optional arg &rest ignore)
-  "`company-mode' completion back-end for `nevermore (nm)'."
-  (interactive (list 'interactive))
-  (let ((case-fold-search t))
-    (pcase command
-      (`interactive (company-begin-backend 'company-nm))
-      (`prefix (and (eq major-mode 'message-mode)
-                    (looking-back "^\\(To\\|Cc\\|Bcc\\):.*"
-                                  (line-beginning-position))
-                    (setq company-nm-last-prefix (company-grab-symbol))))
-      (`candidates (let ((results (completion-substring--all-completions arg nm-completion-addresses nil 0)))
-                     (when results (car results))))
-      (`match (if (string-match company-nm-last-prefix arg)
-                  (match-end 0)
-                0))
-      (`no-cache t))))
-
-(require 'message)
-(add-hook 'message-mode-hook '(lambda ()
-                                (company-mode)
-                                (make-local-variable 'company-backends)
-                                (setq company-backends '(company-nm))))
 
 (provide 'nm)
