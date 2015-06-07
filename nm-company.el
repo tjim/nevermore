bb683531ea2b1621d9a62fc09494213a9ff95f70
Trevor Jim <tjim@mac.com>
2015-06-07 17:47:17 -0400

    Completion: fix for ignoring case

--- nm-company.el
+++ nm-company.el
@@ -11,19 +11,21 @@
 (defun nm-company (command &optional arg &rest ignore)
   "`company-mode' completion back-end for `nevermore (nm)'."
   (interactive (list 'interactive))
-  (let ((case-fold-search t))
-    (pcase command
-      (`interactive (company-begin-backend 'nm-company))
-      (`prefix (and (eq major-mode 'message-mode)
-                    (looking-back "^\\(To\\|Cc\\|Bcc\\):.*"
-                                  (line-beginning-position))
-                    (setq nm-company-last-prefix (company-grab-symbol))))
-      (`candidates (let ((results (completion-substring--all-completions arg nm-completion-addresses nil 0)))
-                     (when results (car results))))
-      (`match (if (string-match nm-company-last-prefix arg)
+  (pcase command
+    (`interactive (company-begin-backend 'nm-company))
+    (`prefix (and (eq major-mode 'message-mode)
+                  (looking-back "^\\(To\\|Cc\\|Bcc\\):.*"
+                                (line-beginning-position))
+                  (setq nm-company-last-prefix (company-grab-symbol))))
+    (`candidates (let* ((completion-ignore-case t)
+                        (results (completion-substring--all-completions arg nm-completion-addresses nil 0)))
+                   (when results (car results))))
+    (`match (let ((case-fold-search t))
+              (if (string-match nm-company-last-prefix arg)
                   (match-end 0)
-                0))
-      (`no-cache t))))
+                0)))
+    (`ignore-case t)
+    (`no-cache t)))
 
 (add-hook 'message-mode-hook '(lambda ()
                                 (company-mode)
